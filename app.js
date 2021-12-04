1581960a6df9bc62845d766c84476bf6c5c27ab7
NEFEDOV_IT <nefedov_it@mail.ru>
2021-12-05 00:34:42 +0300

    fix bag and add new const and rename function

--- app.js
+++ app.js
@@ -1,76 +1,57 @@
-import {BUTTONS_ADD, DELETE, CHECKBOXES, LISTS} from './view.js'
+import {BUTTONS_ADD, DELETE, CHECKBOXES, TASKS} from './view.js'
 
-for (const add of BUTTONS_ADD) {
-    add.addEventListener('click', addTask)
-    add.addEventListener('click', function (e) {
+for (let i = 0; i < BUTTONS_ADD.length; i++) {
+
+    BUTTONS_ADD[i].addEventListener('click', function (e) {
         e.preventDefault()
     })
-}
 
-function addTask() {
-    const TASK_HIGH = document.getElementById('high')
-    const TASK_LOW = document.getElementById('low')
+    BUTTONS_ADD[i].addEventListener('click', function () {
+        const task = TASKS[i]
+        const div = document.createElement('div')
+        const label = document.createElement('label')
+        const input = document.createElement('input')
+        const button = document.createElement('input')
+        const isEmptyTask = task.value.trim()
 
-    const div = document.createElement('div')
-    const label = document.createElement('label')
-    const input = document.createElement('input')
-    const button = document.createElement('input')
+        if (!isEmptyTask) {
+            task.value = ''
+            return task.classList.add('error')
+        }
 
-    if (TASK_HIGH.value === '' && TASK_LOW.value === '') {
-        return alert('Введите задачу')
-    }
+        div.className = 'input-todo'
+        label.className = 'text__input-body'
+        label.textContent = task.value
+        task.value = ''
+        input.className = 'input__checkbox'
+        input.type = 'checkbox'
+        input.addEventListener('click', changeStatus)
+        button.className = 'button_close'
+        button.type = 'button'
+        button.addEventListener('click', deleteTask)
+        task.classList.remove('error')
 
-    div.className = 'input-todo'
-    label.className = 'text__input-body'
-    input.className = 'input__checkbox'
-    input.type = 'checkbox'
-    button.className = 'button_close'
-    button.type = 'button'
-
-    if (TASK_HIGH.value) {
-        label.textContent = TASK_HIGH.value
-        TASK_HIGH.value = ''
-    }
-    if (TASK_LOW.value) {
-        label.textContent = TASK_LOW.value
-        TASK_LOW.value = ''
-    }
-
-    for (const list of LISTS) {
-        this.after(div)
+        task.after(div)
         div.append(label)
         div.append(input)
         div.append(button)
-    }
-
-    const DELETE = document.querySelectorAll('.button_close')
-
-    for (const deleted of DELETE) {
-        deleted.addEventListener('click', deleteTask)
-    }
-
-    const CHECKBOXES = document.querySelectorAll('.input__checkbox')
-
-    for (const checkbox of CHECKBOXES) {
-        checkbox.addEventListener('click', taskDone)
-    }
+    })
 }
+
 
 for (const deleted of DELETE) {
     deleted.addEventListener('click', deleteTask)
 }
 
 for (const checkbox of CHECKBOXES) {
-    checkbox.addEventListener('click', taskDone)
+    checkbox.addEventListener('click', changeStatus)
 }
 
 function deleteTask() {
-    for (const list in LISTS) {
-        this.parentElement.remove()
-    }
+    this.parentElement.remove()
 }
 
-function taskDone() {
+function changeStatus() {
     this.parentElement.classList.toggle('checked__back')
 }
 
